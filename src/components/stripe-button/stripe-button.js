0b50ae1cc2dcfa7f30905bc0ee88a95c01fb3d05
Jenny Hu <orcas03030@gmail.com>
2020-07-06 13:09:14 -0700

    adding stripe integration and updating title

---
+++ src/components/stripe-button/stripe-button.js
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51H1zb0EhQD6o8M8uSehBlZcCWVQ3BcC48YyAOScOxrsBf0mSpXPq9E2bkEKxdp18HugQanURY2zgTV9TewGPoxVX00M0bwIXVE";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
